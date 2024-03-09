c7d013b6e3319eafc2d415abcf27ee73b4851c17
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2024-03-10 01:43:46 +0530

    signup page created

---
+++ frontend/signup.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", register);
+  });
+  
+  function register() {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(name,email,password)
+  
+    axios
+      .post("http://localhost:3000/user/register", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  }
+  
