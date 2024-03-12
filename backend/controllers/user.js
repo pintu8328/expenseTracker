5ea5d27a59825a9c21bdf3489cd3d2600e9fff4e
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2024-03-12 22:12:34 +0530

    password hashed

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,14 +1,17 @@
 const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
+const salt = 10;
 exports.registerUser = (req, res) => {
   const { name, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, salt);
   console.log(req.body)
   User.findOne({ where: { email: email } })
     .then(existingUser => {
       if (existingUser) {
         return res.status(400).json({ message: "Email already registered" });
       } else {
-        User.create({ name, email, password })
+        User.create({ name, email, password: hashedPassword })
           .then((result) => {
             return res.send(result);
           })
@@ -32,13 +35,12 @@ exports.loginUser = (req, res) => {
           });
         }
 
-        if (result.password !== password) {
-            return res.status(401).send({
-              message: "Invalid password",
-            });
-          }
+        if (!bcryptjs.compareSync(password, result.dataValues.password)) {
+          return res.status(401).send({
+            message: "Password is incorrect",
+          });
+        }
 
-  
         return res.status(200).send({
           success: true,
           message: "Login Success",
